c180dfe85403af0355741de822bb2af32b433987
twodadoos <197021348+twodadoos@users.noreply.github.com>
2026-01-13 21:03:40 -0500

    Added convert-epochs.sh

---
+++ linux/convert-epochs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#############################################
+################### Usage ###################
+#
+#      ./convert-epoch.sh input.log
+#
+#############################################
+
+set -euo pipefail
+
+input_file="$1"
+output_file="${input_file}.converted"
+
+while read -r line; do
+  epoch=${line%% *}                     # first field
+  remainder_of_line=${line#* }          # remainder of line
+
+  human_readable_date=$(date -d "@${epoch%.*}" "+%Y-%m-%d %H:%M:%S")
+
+  printf '%s %s\n' "$human_readable_date" "$remainder_of_line"
+done < "$input_file" > "$output_file"
